5e799c25c20d69b6e36b41285b7c7dfec5c4d454
CarlaVasconcelos <carlajulyanne@outlook.com>
2023-05-15 09:14:30 -0300

    new commit

---
+++ src/modules/login/screens/Login.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import {ContainerLogin} from "../styles/login.style"
+
+const Login = () => {
+    return(
+        <View>
+            <ContainerLogin />
+            <Text>ahuahu</Text>
+        </View>
+    )
+}
+
+export default Login;
